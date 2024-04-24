ac2240c21aeb198b658f35005e0749ae94dc786e
Icaro-Lima <icarodantas.lima@gmail.com>
2024-04-24 15:59:45 -0300

    Remove campaing tracking url

--- src/GooglePlayButton.tsx
+++ src/GooglePlayButton.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function GooglePlayButton(props: { style?: React.CSSProperties }) {
   return (
-    <a href="https://play.google.com/store/apps/details?id=com.icarodlima.easylapse3d&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">
+    <a href="https://play.google.com/store/apps/details?id=com.icarodlima.easylapse3d">
       <img
         style={props.style}
         alt="Get it on Google Play"
