c46aafb952fbf58af70e8c055dd7ac92abdb663b
Icaro-Lima <icarodantas.lima@gmail.com>
2024-05-14 23:07:00 -0300

    Update metadata base url

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Providers } from "./providers";
 
 export const metadata = {
+  metadataBase: new URL("https://3d-easylapse.github.io"),
   title: "3D EasyLapse",
   description:
     "3D EasyLapse - Capture those fancy time-lapses of your 3D printings using just a phone!",
